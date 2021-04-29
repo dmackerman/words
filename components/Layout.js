80558efce6c8e46104ff3abf6b884af149c82f47
Dave Ackerman <dave@kentik.com>
2021-04-29 13:33:28 -0700

    google fonts

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,11 @@
 import Head from "next/head";
 import Header from "./Header";
+import { GoogleFonts } from "next-google-fonts";
 
 export default function Layout({ children, pageTitle, description }) {
   return (
     <>
+      <GoogleFonts href="https://fonts.googleapis.com/css2?family=Overpass:wght@400;700&display=swap" />
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
