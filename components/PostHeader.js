513e1631417467f3862b42cb6bd1c20e4c887a54
Dave Ackerman <dave@kentik.com>
2021-04-29 13:37:54 -0700

    cool

--- components/PostHeader.js
+++ components/PostHeader.js
@@ -7,7 +7,7 @@ export default function PostHeader({ meta }) {
         <title>{meta.title}</title>
       </Head>
       <h1>{meta.title}</h1>
-      <div className="details">
+      <div className="meta">
         <p>{meta.description}</p>
         <span>{meta.date}</span>
       </div>
