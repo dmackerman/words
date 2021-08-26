a2e85c86b01d84b9324b8e1ec1a5d37e14c41165
Dave Ackerman <dmackerman@gmail.com>
2021-08-26 11:59:47 -0700

    stuff i guess

--- components/Layout.js
+++ components/Layout.js
@@ -5,7 +5,7 @@ import { GoogleFonts } from "next-google-fonts";
 export default function Layout({ children, pageTitle, description }) {
   return (
     <>
-      <GoogleFonts href="https://fonts.googleapis.com/css2?family=Overpass:wght@400;700&display=swap" />
+      <GoogleFonts href="https://fonts.googleapis.com/css2?family=Overpass:wght@100;400;700&display=swap" />
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
